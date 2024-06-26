41a18ca7e5f24f442f55e40f4cdeee8c711463ed
soongu <naong526@naver.com>
2024-06-26 10:26:23 +0900

    # 7. JPA Sorting

--- src/main/java/com/spring/jpastudy/chap03_page/StudentPageRepository.java
+++ src/main/java/com/spring/jpastudy/chap03_page/StudentPageRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface StudentPageRepository extends JpaRepository<Student, String> {
 
     // 전체조회 상황에서 페이징 처리하기
-    Page<Student> findAll(Pageable pageable);
+//    Page<Student> findAll(Pageable pageable);
 
     // 검색 + 페이징
     Page<Student> findByNameContaining(String name, Pageable pageable);
