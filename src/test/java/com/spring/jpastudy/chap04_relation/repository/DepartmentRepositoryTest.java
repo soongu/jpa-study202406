845f4a88ae71b487ccccee8a7b5ecda5cb4fe9b2
soongu <naong526@naver.com>
2024-06-26 12:04:56 +0900

    # 10. JPA 양방향 관계에서 데이터 갱신이 반대편에 반영되지 않는 문제

--- src/test/java/com/spring/jpastudy/chap04_relation/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/spring/jpastudy/chap04_relation/repository/DepartmentRepositoryTest.java
@@ -49,4 +49,47 @@ class DepartmentRepositoryTest {
     }
 
 
+    // 양방향 연관관계에서 리스트에 데이터 갱신시 주의사항
+
+    @Test
+    @DisplayName("양방향 연관관계에서 연관데이터 수정")
+    void changeTest() {
+        //given
+
+        // 3번 사원의 부서를 2번부서에서 1번부서로 수정
+
+        // 3번 사원 정보 조회
+        Employee employee = employeeRepository.findById(3L).orElseThrow();
+
+        // 1번 부서 정보 조회
+        Department department = departmentRepository.findById(1L).orElseThrow();
+
+        //when
+
+        /*
+            사원정보가 Employee엔터티에서 수정되어도
+            반대편 엔터티인 Department에서는 리스트에 바로 반영되지 않는다.
+
+            해결방안은 데이터 수정시에 반대편 엔터티에도 같이 수정을 해줘라
+         */
+        // 사원정보 수정
+//        employee.setDepartment(department);
+//
+//        // 핵심: 양방향에서는 수정시 반대편도 같이 수정
+//        department.getEmployees().add(employee);
+
+        employee.changeDepartment(department);
+
+        employeeRepository.save(employee);
+
+        //then
+        // 바뀐부서의 사원목록 조회
+        List<Employee> employees = department.getEmployees();
+        System.out.println("\n\n\n");
+        employees.forEach(System.out::println);
+        System.out.println("\n\n\n");
+    }
+
+
+
 }
